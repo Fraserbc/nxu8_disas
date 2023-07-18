3406b2d8d14d8a345911820933680adfa5ed51c7
Fraser Price <fraserbcprice@gmail.com>
2023-07-18 18:27:49 +0200

    Formatting improvements and relative jumps

--- src/main.c
+++ src/main.c
@@ -18,14 +18,14 @@ int main(int argc, char **argv) {
 		struct nxu8_instr *instr = nxu8_decode_instr(decoder, addr);
 
 		if (instr != NULL) {
-			printf("0x%08x    ", instr->addr);
-			for (int x = 0; x < instr->len; x += 2) printf("%04x", nxu8_read16(decoder, addr + x));
-			if (instr->len < 4) printf("    ");
-			printf("    %s\n", instr->assembly);
+			printf("%05Xh\t", instr->addr);
+			for (int x = 0; x < instr->len; x += 2) printf("%04X", nxu8_read16(decoder, addr + x));
+			if (instr->len < 4) printf("\t");
+			printf("\t%s\n", instr->assembly);
 			addr += instr->len;
 		} else {
 			uint16_t val = nxu8_read16(decoder, addr);
-			printf("0x%08x    %04x        dw %04x\n", addr, val, val);
+			printf("%05X\t%04X\t\tdw %04x\n", addr, val, val);
 			addr += 2;
 		}
 	}
